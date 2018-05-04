ac60b6a292180e01317383298201da03922b60ff
Jac08H <jac08h@protonmail.com>
2018-05-04 20:06:56 +0200

    created Article class
    moved functions from main file to Article class
    'find_path_to_article' - try to find path to article by choosing random article from hyperlinks found on page.

---
+++ article.py
@@ -0,0 +1,111 @@
+from bs4 import BeautifulSoup
+from random import choice
+from urllib.request import urlopen
+from urllib.parse import unquote
+import urllib.error
+import argparse
+from sys import exit
+
+
+def article_from_url(url, site):
+    article = url.replace(site + '/wiki/', '')
+    article = article.replace('_', ' ')
+    article = unquote(article)
+
+    return article
+
+
+def article_to_url(article, site):
+    article = article.replace(' ', '_')
+    url = ''.join([site, '/wiki/', article])
+    return url
+
+
+class Article:
+    def __init__(self, name, url, site):
+        self.name = name
+        self.url = url
+        self.site = site
+
+    def get_hyperlinks(self):
+        try:
+            result = urlopen(self.url)
+        except urllib.error.HTTPError:
+            print(self.url)
+            print("[ERROR]: Page '{}' not found. Exiting.".format(article_from_url(self.url, self.site)))
+            exit()  # sys.exit()
+
+        soup = BeautifulSoup(result, "html.parser")
+
+        all_links = [link.get('href') for link in soup.find_all('a')]
+        links = []
+
+        for link in all_links:
+            if link is not None and link[0] == '/' and not(any((c in link) for c in '.:#')) and 'Main_Page' not in link:
+                # link[0] != '/' --> offsite link
+                # '.' in link    --> /wiki/File:Slovakia_Trnava_Town_Hall.jpg
+                # ':' in link    --> /wiki/Category:Slavic_countries_and_territories
+                # '#' in link    --> categories
+
+                links.append(self.site + link)
+
+        return links
+
+    def find_path_to_article(self, article, path=[], limit=20, depth=0):
+        """
+        Try to find path to article by choosing random article from hyperlinks found on page.
+
+        :param Article article: Article object of target article
+        :param list path: list to prevent visiting articles multiple times
+        :param int limit: prevent searching further than this value
+        :param int depth: track length of the path
+
+        :returns: path if the path was found in less then `limit` steps, else None
+        :rtype: list or NoneType
+        """
+        links = self.get_hyperlinks()
+
+        if article.url in links:
+            return path
+
+        elif depth > limit:
+            return None
+
+        else:
+            link = choice(links)
+            name = article_from_url(link, site)
+
+            while name in path:
+                link = choice(links)
+                name = article_from_url(link, site)
+
+            next_article = Article(name, link, self.site)
+            path.append(name)
+            return next_article.find_path(article, path, depth=depth+1)
+
+    def random_walk(self, steps):
+        """
+        Find random path by randomly choosing next article from hyperlinks found on page.
+
+        :param int steps: number of jumps between articles
+
+        :return: last visited url
+        :rtype: str
+        """
+        url = self.url
+        name = self.name
+        visited = []
+
+        for i in range(1, steps + 1):
+            name = article_from_url(url, self.site)
+            next_article = Article(name, url, self.site)
+            visited.append(url)
+
+            print('{}. step: {} ({})'.format(i, name, url))
+            url = None
+            while url in visited or url is None:
+                url = choice(next_article.get_hyperlinks())
+
+        print('{} -- {} --> {}'.format(self.name, steps, name))
+
+        return url
--- six_degrees_of_wikipedia.py
+++ six_degrees_of_wikipedia.py
@@ -1,76 +1,8 @@
 #!/usr/bin/env python
 
-from bs4 import BeautifulSoup
-from random import choice
-from urllib.request import urlopen
-from urllib.parse import unquote
-import urllib.error
-import argparse
-from sys import exit
-
-
-def url_to_article(url):
-    article = url.replace('https://en.wikipedia.org/wiki/', '')
-    article = article.replace('_', ' ')
-    article = unquote(article)
-
-    return article
-
-
-def article_to_url(article):
-    url = article.replace(' ', '_')
-    return url
-
-
-def find_random_article(url):
-    try:
-        result = urlopen(url)
-    except urllib.error.HTTPError:
-        print("[ERROR]: Page '{}' not found. Exiting.".format(url_to_article(url)))
-        exit()
-
-    soup = BeautifulSoup(result, "html.parser")
-
-    links = [link.get('href') for link in soup.find_all('a')]
-
-    link = '#'
-    while link is None or link[0] != '/' or any((c in link) for c in '.:'):
-        # link[0] != '/' --> offsite link
-        # '.' in link    --> /wiki/File:Slovakia_Trnava_Town_Hall.jpg
-        # ':' in link    --> /wiki/Category:Slavic_countries_and_territories
-
-        link = choice(links)
-
-    link = 'https://en.wikipedia.org' + link
-
-    return link
-
-
-def traverse(start_article, number_of_steps, verbose=True):
-    url = 'https://en.wikipedia.org/wiki/' + article_to_url(start_article)
-
-    print("Starting {} degrees of wikipedia from '{}'".format(number_of_steps, start_article))
-
-    for i in range(1, number_of_steps + 1):
-        url = find_random_article(url)
-        if verbose:
-            print('{}. step: {}'.format(i, url_to_article(url)))
-
-    print('{} -- {} --> {}'.format(start_article, number_of_steps, url_to_article(url)))
-
-    return url
-
 
 def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('article', help='article to start from')
-    parser.add_argument('steps', help='number of steps to take.', type=int)
-    parser.add_argument('-v', '--verbosity', help='increase verbosity', action='store_true')
-
-    args = parser.parse_args()
-
-    result = traverse(args.article, args.steps, args.verbosity)
-
+    pass
 
 if __name__ == '__main__':
     main()
